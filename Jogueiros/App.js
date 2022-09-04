c72889bea5807667c44b9789a9c9c0ca160c3019
Rafael Viktor <rafaellviktor@gmail.com>
2022-09-04 01:51:19 -0300

    Basic app made

--- Jogueiros/App.js
+++ Jogueiros/App.js
@@ -1,20 +1,69 @@
+import * as React from 'react';
+import { StyleSheet, Button, View, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+function HomeExplorar() {
   return (
     <View style={styles.container}>
-      <Text>Bem-vindo</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Algum local de preferência?' style={styles.inputpesquisa} />
     </View>
+  );
+}
+
+function PerfilLogin() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+      <Text style={styles.loginh1}>Entre ou cadastre-se no Jogueiros.com</Text>
+      <TextInput placeholder='Número de telefone' style={styles.input}/>
+      <Text style={styles.label}>Não iremos entrar em contato, é apenas para verificar se você já possui login.</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Explorar" component={HomeExplorar} />
+        <Tab.Screen name="Perfil" component={PerfilLogin} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft: 30,
+    marginRight: 30
   },
+  loginh1: {
+    marginTop: 40,
+    fontWeight: 'bold',
+    fontSize: 30
+  },
+  label: {
+    marginTop: 10
+  },
+  input: {
+    marginTop: 40,
+    backgroundColor: '#EAEAEA',
+    padding: 10,
+    borderRadius: 10
+  },
+  inputpesquisa: {
+    marginTop: 40,
+    backgroundColor: '#EAEAEA',
+    padding: 10,
+    borderRadius: 10,
+    textAlign: 'center'
+  }
 });
+
+export default App;
