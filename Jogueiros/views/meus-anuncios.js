51e4fc0fd9df664089eff29aeaa80b59ff946305
Rafael Viktor <rafaellviktor@gmail.com>
2022-10-06 21:21:34 -0300

    home, populares e meus anúncios criado

--- Jogueiros/views/meus-anuncios.js
+++ Jogueiros/views/meus-anuncios.js
@@ -1,102 +1,141 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
-import { TextInputMask } from 'react-native-masked-text';
-import Button from '../assets/components/button';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, ScrollView, Pressable, Image, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import api from '../assets/api/axios';
 
-function EditarPerfil({ navigation }) {
-  const CADASTRO_URL = '/users/registrar';
+function MeusAnuncios({ navigation }) {
+  const MEUSANUNCIOS_URL = '/perfil/meus-anuncios';
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
-  const [contato, setContato] = useState('');
-  const celRef = useRef(null);
+  const [anuncios, setAnunciosObj] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
-  const Registrar = async () => {
-    const contatoNoMask = celRef?.current.getRawValue();
-    const data = {
-      nome: nome,
-      email: email,
-      senha: senha,
-      contato: contatoNoMask
-    }
+  const getToken = async () => {
     try {
-      const response = await api.post(CADASTRO_URL,JSON.stringify(data),
-        {
-          headers: {'Content-Type':'application/json'},
-          withCredentials: true
-        });
-        if(response.data.success === true) {
-          AsyncStorage.setItem('@TOKEN', response.data.result)
-          Keyboard.dismiss();
-          navigation.goBack();
-          alert(response.data.message)
-        } else if(response?.data.success === false) {
-          alert(response.data.message)
-        } else {
-          alert(response.data.message)
-        }
+      const token = await AsyncStorage.getItem('token');
+      const response = await api.get(MEUSANUNCIOS_URL,{
+        headers: {'Content-Type':'application/json','token':`${token}`},
+        withCredentials: true
+      });
+      if(response.data.success === true) {
+        setAnunciosObj(response.data.result)
+        setIsAuthenticated(true)
+      }
     } catch (err) {
-        console.error(err)
+      setIsAuthenticated(false);
+      navigation.goBack();
+      await AsyncStorage.setItem('token', '');
     }
+    setIsLoading(false)
   }
 
-  return (
-    <View style={styles.root}>
-      <Text style={styles.registerh1}>Insira seus dados para finalizar o cadastro </Text>
-      <View style={styles.container}>
-        <TextInput placeholder='Nome' style={styles.inputText} value={nome} onChangeText={setNome}/>
-        <TextInput placeholder='E-Mail' style={styles.inputText} value={email} onChangeText={setEmail}/>
-        <TextInput placeholder='Senha' style={styles.inputText} value={senha} onChangeText={setSenha} secureTextEntry/>
-        <TextInputMask placeholder='Celular' style={styles.inputContato} type={'cel-phone'} value={contato} ref={celRef} onChangeText={setContato}/>
-        <Button title='Criar conta' onpress={Registrar} style={{ width: '100%' }}/>
-        <Button title='Já possui conta? Faça login' onpress={() => navigation.goBack()} type='tertiary'/>
-      </View>
+  useEffect(() => {
+    getToken();
+  },[])
+
+  if(isLoading === false) {
+    return (
+      <View style={styles.root}>
+      <ScrollView overScrollMode='never'>
+        <View style={styles.containerCards}>
+          {
+            anuncios && anuncios.map((item, index) => (
+            <Pressable key={index}>
+              <Image style={styles.cardImage} source={{uri : `https://jogueiros-api.herokuapp.com/uploads/${item.imagem}`}}></Image>
+              <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                  <Text style={styles.cardTitle}>{item.titulo}</Text>
+                  <Text style={styles.cardSubtitle}>{item.visualizacoes} Visualizações</Text>
+                </View>
+                <Text style={styles.cardPrice}>{item.preco} R$ /hora</Text>
+              </View>
+            </Pressable>
+            ))
+          }
+        </View>
+      </ScrollView>
     </View>
-  );
+    );
+} else {
+    return (
+    <View style={styles.center}>
+      <ActivityIndicator color={"#1a9946"} size={60} />
+    </View>)
+  }
 }
 
-  const styles = StyleSheet.create({
-    root: {
-      flex: 1,
-      marginLeft: 15,
-      marginRight: 15
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    registerh1: {
-      fontWeight: 'bold',
-      fontSize: 30,
-      marginTop: '10%'
-    },
-    subtitle: {
-      fontWeight: 'bold',
-      fontSize: 15,
-      marginTop: '5%'
-    },
-    label: {
-      marginTop: 20
-    },
-    inputText: {
-      width: '100%',
-      marginTop: 5,
-      marginBottom: 5,
-      backgroundColor: '#EAEAEA',
-      padding: 10,
-      borderRadius: 10
-    },
-    inputContato: {
-      width: '100%',
-      marginTop: 5,
-      marginBottom: 20,
-      backgroundColor: '#EAEAEA',
-      padding: 10,
-      borderRadius: 10
-    }
-  });
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    marginTop: 28,
+    paddingLeft: 18,
+    paddingRight: 18,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderColor: '#e8e8e8'
+  },
+  logoImage: {
+    width: '101%',
+    height: 65,
+  },
+  searchSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  searchIcon: {
+    padding: 10,
+    backgroundColor: '#EAEAEA',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10
+  },
+  inputpesquisa: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#EAEAEA',
+    color: '#424242',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
+  containerCards: {
+    marginLeft: 18,
+    marginRight: 18
+  },
+  cardImage: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: '100%',
+    height: 250,
+    borderRadius: 15
+  },
+  cardTitle: {
+    marginLeft: 8,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  cardSubtitle: {
+    marginLeft: 8,
+    color: '#878787',
+    fontSize: 13,
+    fontWeight: 'bold'
+  },
+  cardPrice: {
+    marginRight: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#1a9946'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
-export default EditarPerfil;
+export default MeusAnuncios;
--- Jogueiros/views/populares.js
+++ Jogueiros/views/populares.js
@@ -1,10 +1,41 @@
-import * as React from 'react';
-import { StyleSheet, View, ScrollView, Text, TextInput, Image, Pressable } from 'react-native';
-import Ionic from 'react-native-vector-icons/Ionicons'
-
-import logo from '../assets/jogueiros-logo.png'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, ScrollView, Text, TextInput, Image, Pressable, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import logo from '../assets/jogueiros-logo.png';
+import api from '../assets/api/axios';
 
 function Populares() {
+  const ANUNCIOS_URL = '/anuncios/populares';
+
+  const [anuncios, setAnunciosObj] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  const getToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      const response = await api.get(ANUNCIOS_URL,{
+        headers: {'Content-Type':'application/json','token':`${token}`},
+        withCredentials: true
+      });
+      if(response.data.success === true) {
+        setAnunciosObj(response.data.result)
+        setIsAuthenticated(true)
+      }
+    } catch (err) {
+      setIsAuthenticated(false);
+      navigation.goBack();
+      await AsyncStorage.setItem('token', '');
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getToken();
+  },[])
+
+  if(isLoading === false) {
     return (
       <View style={styles.root}>
         <View style={styles.container}>
@@ -16,60 +47,42 @@ function Populares() {
         </View>
         <ScrollView overScrollMode='never'>
           <View style={styles.containerCards}>
-          <Pressable>
-            <Image style={styles.cardImage} source={{uri : 'https://lh3.googleusercontent.com/fife/AAbDypDw2iRVXG1DXpZvRtg2PxE-jxPcBE_o3bX80SIFD6-Xn0d4IwOBTsqUddoSx9HUQXx0cxP5IPHW0GG1ZrtSnru6N-LAopA-C57RU673GF70DUcVJ9h6nrWUjxA_er3IU1AEgyqasKnWkdk72EeeFe7j4J3pR297WFGX7c_TBZ8TeY2HQOzZ9Ufr4SnMyWsaoCMWfaduANgYwamAEreYivR84Bj8MizSoKL9SXXPCH6G4VZ_ggNTU5TCAUjqYqnn7hxalHS4O2kfNal-Up9Tks3CCBuvFvE_TdYSxdaHaE9oV48ssh-7szXLw0EAGTBymcWkJD5v-pbbe4-4wCn8A24Eq4dL_7YMlRaLXYB6xsPWEZ-YRzCtQe6vfKaKsX0elwM5dGdPw8LHmirCcljsCbwch35F2zPJmawNTJDQ5hrZtfYmuGE9FLK0rjAQS-H4fyQhcW0cHSQZevirItDfVJe748nbByRuZ4qpODJMa0yd3Tfvd6WoAlkmt1R3hFDgbBZQ7rGvdjpjyov_uDvbuI31EHNdqYffHH5hHT1Eq61EbtyXUuX9tCpRAxzMjOmTc1kWE0huA-ogb9lViTyBoY-z318VIPylU_K9iZtjgKbZtT6izBEtxB56OVLjVt_fPxDRMvTFIDekeOUAlJi0Y_jYw923t_P4__eQWTYLDY7YqjX6py56_BVxzSwAjvTIPErE1uWpgl2gQlstw10rmzKR9UpX23w184OH0_l48tcGTwbCBAvkkNTIlE-6VRvN7V3LyhAZVlcOa-Bb2Ct3ykhpZ075ya6wDRyd2jJRfZdV1I3tL_v5XwtPurOp5n6yjk0uaJwTY1e5SdvwYapBgCYW4njU3Ik4MoLG2YvpN36GeR44bjc8oGdfcLQHV1mBof6eOQ9mB6XSrf6KBmDrlQu4sCJ7YhNYEmjcfpmMzRVdVwMuMlH2HzxcFy8LhN6od5tXTiTGN4Xi3cx92JmQhzK2HMoocGO_YBgGDQTWyYIGHElFMfsAQxO53EzN_I231Rz89ysgRM5ATaJOG640xf0sy59vIOi7CePURKCOOgBS9yZrcCkjxXYnWCiQUHdPG3u8jIpwL7uP8JS9Nvn1Fh15lc3MnKagqjkTGKJvMp2Q9QTPnhbcpeyJkBWTN-ShNBe3WHsVFxT4I012qt11LzRj4I0_mxmJ1I9KQQY0GgvnOWEyEe_QKo_XFvryzsfT0du6WEi7Qaty4QqmbxkMxygjgbejilHzzNgv0q6C30cdiV12ZaGf-4wShrEqh9C0Z89-PnA5dI6lAa2yURmf1P-6M6BhRRE4NjP6SSlZdIQ=w1879-h962'}}></Image>
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <Text style={styles.cardTitle}>Bom de Bola Society</Text>
-                <Text style={styles.cardSubtitle}>10 Visualizações</Text>
+          {
+            anuncios && anuncios.map((item, index) => (
+            <Pressable key={index}>
+              <Image style={styles.cardImage} source={{uri : `https://jogueiros-api.herokuapp.com/uploads/${item.imagem}`}}></Image>
+              <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                  <Text style={styles.cardTitle}>{item.titulo}</Text>
+                  <Text style={styles.cardSubtitle}>{item.visualizacoes} Visualizações</Text>
+                </View>
+                <Text style={styles.cardPrice}>{item.preco} R$ /hora</Text>
               </View>
-              <Text style={styles.cardPrice}>100 R$ /hora</Text>
-            </View>
-          </Pressable>
-
-          <Pressable>
-            <Image style={styles.cardImage} source={{uri : 'https://lh3.googleusercontent.com/fife/AAbDypDw2iRVXG1DXpZvRtg2PxE-jxPcBE_o3bX80SIFD6-Xn0d4IwOBTsqUddoSx9HUQXx0cxP5IPHW0GG1ZrtSnru6N-LAopA-C57RU673GF70DUcVJ9h6nrWUjxA_er3IU1AEgyqasKnWkdk72EeeFe7j4J3pR297WFGX7c_TBZ8TeY2HQOzZ9Ufr4SnMyWsaoCMWfaduANgYwamAEreYivR84Bj8MizSoKL9SXXPCH6G4VZ_ggNTU5TCAUjqYqnn7hxalHS4O2kfNal-Up9Tks3CCBuvFvE_TdYSxdaHaE9oV48ssh-7szXLw0EAGTBymcWkJD5v-pbbe4-4wCn8A24Eq4dL_7YMlRaLXYB6xsPWEZ-YRzCtQe6vfKaKsX0elwM5dGdPw8LHmirCcljsCbwch35F2zPJmawNTJDQ5hrZtfYmuGE9FLK0rjAQS-H4fyQhcW0cHSQZevirItDfVJe748nbByRuZ4qpODJMa0yd3Tfvd6WoAlkmt1R3hFDgbBZQ7rGvdjpjyov_uDvbuI31EHNdqYffHH5hHT1Eq61EbtyXUuX9tCpRAxzMjOmTc1kWE0huA-ogb9lViTyBoY-z318VIPylU_K9iZtjgKbZtT6izBEtxB56OVLjVt_fPxDRMvTFIDekeOUAlJi0Y_jYw923t_P4__eQWTYLDY7YqjX6py56_BVxzSwAjvTIPErE1uWpgl2gQlstw10rmzKR9UpX23w184OH0_l48tcGTwbCBAvkkNTIlE-6VRvN7V3LyhAZVlcOa-Bb2Ct3ykhpZ075ya6wDRyd2jJRfZdV1I3tL_v5XwtPurOp5n6yjk0uaJwTY1e5SdvwYapBgCYW4njU3Ik4MoLG2YvpN36GeR44bjc8oGdfcLQHV1mBof6eOQ9mB6XSrf6KBmDrlQu4sCJ7YhNYEmjcfpmMzRVdVwMuMlH2HzxcFy8LhN6od5tXTiTGN4Xi3cx92JmQhzK2HMoocGO_YBgGDQTWyYIGHElFMfsAQxO53EzN_I231Rz89ysgRM5ATaJOG640xf0sy59vIOi7CePURKCOOgBS9yZrcCkjxXYnWCiQUHdPG3u8jIpwL7uP8JS9Nvn1Fh15lc3MnKagqjkTGKJvMp2Q9QTPnhbcpeyJkBWTN-ShNBe3WHsVFxT4I012qt11LzRj4I0_mxmJ1I9KQQY0GgvnOWEyEe_QKo_XFvryzsfT0du6WEi7Qaty4QqmbxkMxygjgbejilHzzNgv0q6C30cdiV12ZaGf-4wShrEqh9C0Z89-PnA5dI6lAa2yURmf1P-6M6BhRRE4NjP6SSlZdIQ=w1879-h962'}}></Image>
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <Text style={styles.cardTitle}>Bom de Bola Society</Text>
-                <Text style={styles.cardSubtitle}>10 Visualizações</Text>
-              </View>
-              <Text style={styles.cardPrice}>100 R$ /hora</Text>
-            </View>
-          </Pressable>
-
-          <Pressable>
-            <Image style={styles.cardImage} source={{uri : 'https://lh3.googleusercontent.com/fife/AAbDypDw2iRVXG1DXpZvRtg2PxE-jxPcBE_o3bX80SIFD6-Xn0d4IwOBTsqUddoSx9HUQXx0cxP5IPHW0GG1ZrtSnru6N-LAopA-C57RU673GF70DUcVJ9h6nrWUjxA_er3IU1AEgyqasKnWkdk72EeeFe7j4J3pR297WFGX7c_TBZ8TeY2HQOzZ9Ufr4SnMyWsaoCMWfaduANgYwamAEreYivR84Bj8MizSoKL9SXXPCH6G4VZ_ggNTU5TCAUjqYqnn7hxalHS4O2kfNal-Up9Tks3CCBuvFvE_TdYSxdaHaE9oV48ssh-7szXLw0EAGTBymcWkJD5v-pbbe4-4wCn8A24Eq4dL_7YMlRaLXYB6xsPWEZ-YRzCtQe6vfKaKsX0elwM5dGdPw8LHmirCcljsCbwch35F2zPJmawNTJDQ5hrZtfYmuGE9FLK0rjAQS-H4fyQhcW0cHSQZevirItDfVJe748nbByRuZ4qpODJMa0yd3Tfvd6WoAlkmt1R3hFDgbBZQ7rGvdjpjyov_uDvbuI31EHNdqYffHH5hHT1Eq61EbtyXUuX9tCpRAxzMjOmTc1kWE0huA-ogb9lViTyBoY-z318VIPylU_K9iZtjgKbZtT6izBEtxB56OVLjVt_fPxDRMvTFIDekeOUAlJi0Y_jYw923t_P4__eQWTYLDY7YqjX6py56_BVxzSwAjvTIPErE1uWpgl2gQlstw10rmzKR9UpX23w184OH0_l48tcGTwbCBAvkkNTIlE-6VRvN7V3LyhAZVlcOa-Bb2Ct3ykhpZ075ya6wDRyd2jJRfZdV1I3tL_v5XwtPurOp5n6yjk0uaJwTY1e5SdvwYapBgCYW4njU3Ik4MoLG2YvpN36GeR44bjc8oGdfcLQHV1mBof6eOQ9mB6XSrf6KBmDrlQu4sCJ7YhNYEmjcfpmMzRVdVwMuMlH2HzxcFy8LhN6od5tXTiTGN4Xi3cx92JmQhzK2HMoocGO_YBgGDQTWyYIGHElFMfsAQxO53EzN_I231Rz89ysgRM5ATaJOG640xf0sy59vIOi7CePURKCOOgBS9yZrcCkjxXYnWCiQUHdPG3u8jIpwL7uP8JS9Nvn1Fh15lc3MnKagqjkTGKJvMp2Q9QTPnhbcpeyJkBWTN-ShNBe3WHsVFxT4I012qt11LzRj4I0_mxmJ1I9KQQY0GgvnOWEyEe_QKo_XFvryzsfT0du6WEi7Qaty4QqmbxkMxygjgbejilHzzNgv0q6C30cdiV12ZaGf-4wShrEqh9C0Z89-PnA5dI6lAa2yURmf1P-6M6BhRRE4NjP6SSlZdIQ=w1879-h962'}}></Image>
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-              <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <Text style={styles.cardTitle}>Bom de Bola Society</Text>
-                <Text style={styles.cardSubtitle}>10 Visualizações</Text>
-              </View>
-              <Text style={styles.cardPrice}>100 R$ /hora</Text>
-            </View>
-          </Pressable>
-
-          <Pressable>
-            <Image style={styles.cardImage} source={{uri : 'https://lh3.googleusercontent.com/fife/AAbDypDw2iRVXG1DXpZvRtg2PxE-jxPcBE_o3bX80SIFD6-Xn0d4IwOBTsqUddoSx9HUQXx0cxP5IPHW0GG1ZrtSnru6N-LAopA-C57RU673GF70DUcVJ9h6nrWUjxA_er3IU1AEgyqasKnWkdk72EeeFe7j4J3pR297WFGX7c_TBZ8TeY2HQOzZ9Ufr4SnMyWsaoCMWfaduANgYwamAEreYivR84Bj8MizSoKL9SXXPCH6G4VZ_ggNTU5TCAUjqYqnn7hxalHS4O2kfNal-Up9Tks3CCBuvFvE_TdYSxdaHaE9oV48ssh-7szXLw0EAGTBymcWkJD5v-pbbe4-4wCn8A24Eq4dL_7YMlRaLXYB6xsPWEZ-YRzCtQe6vfKaKsX0elwM5dGdPw8LHmirCcljsCbwch35F2zPJmawNTJDQ5hrZtfYmuGE9FLK0rjAQS-H4fyQhcW0cHSQZevirItDfVJe748nbByRuZ4qpODJMa0yd3Tfvd6WoAlkmt1R3hFDgbBZQ7rGvdjpjyov_uDvbuI31EHNdqYffHH5hHT1Eq61EbtyXUuX9tCpRAxzMjOmTc1kWE0huA-ogb9lViTyBoY-z318VIPylU_K9iZtjgKbZtT6izBEtxB56OVLjVt_fPxDRMvTFIDekeOUAlJi0Y_jYw923t_P4__eQWTYLDY7YqjX6py56_BVxzSwAjvTIPErE1uWpgl2gQlstw10rmzKR9UpX23w184OH0_l48tcGTwbCBAvkkNTIlE-6VRvN7V3LyhAZVlcOa-Bb2Ct3ykhpZ075ya6wDRyd2jJRfZdV1I3tL_v5XwtPurOp5n6yjk0uaJwTY1e5SdvwYapBgCYW4njU3Ik4MoLG2YvpN36GeR44bjc8oGdfcLQHV1mBof6eOQ9mB6XSrf6KBmDrlQu4sCJ7YhNYEmjcfpmMzRVdVwMuMlH2HzxcFy8LhN6od5tXTiTGN4Xi3cx92JmQhzK2HMoocGO_YBgGDQTWyYIGHElFMfsAQxO53EzN_I231Rz89ysgRM5ATaJOG640xf0sy59vIOi7CePURKCOOgBS9yZrcCkjxXYnWCiQUHdPG3u8jIpwL7uP8JS9Nvn1Fh15lc3MnKagqjkTGKJvMp2Q9QTPnhbcpeyJkBWTN-ShNBe3WHsVFxT4I012qt11LzRj4I0_mxmJ1I9KQQY0GgvnOWEyEe_QKo_XFvryzsfT0du6WEi7Qaty4QqmbxkMxygjgbejilHzzNgv0q6C30cdiV12ZaGf-4wShrEqh9C0Z89-PnA5dI6lAa2yURmf1P-6M6BhRRE4NjP6SSlZdIQ=w1879-h962'}}></Image>
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10}}>
-              <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-                <Text style={styles.cardTitle}>Bom de Bola Society</Text>
-                <Text style={styles.cardSubtitle}>10 Visualizações</Text>
-              </View>
-              <Text style={styles.cardPrice}>100 R$ /hora</Text>
-            </View>
-          </Pressable>
+            </Pressable>
+            ))
+          }
           </View>
         </ScrollView>
       </View>
     );
+  } else {
+    return (
+    <View style={styles.center}>
+    <ActivityIndicator color={"#1a9946"} size={60} />
+    </View>)
   }
+}
 
   const styles = StyleSheet.create({
     root: {
       backgroundColor: 'white',
       width: '100%',
       height: '100%',
+    },
+    center: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     container: {
       marginTop: 28,
