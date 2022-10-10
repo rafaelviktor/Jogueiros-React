75575a1b61860fb24f2d424cae9168cc42f75c4d
Rafael <rafaellviktor@gmail.com>
2022-10-10 19:26:41 -0300

    correção de bug

--- Jogueiros/views/populares.js
+++ Jogueiros/views/populares.js
@@ -51,7 +51,7 @@ function Populares({ navigation }) {
           <View style={styles.containerCards}>
           {
             anuncios && anuncios.map((item, index) => (
-            <Pressable key={index} onPress={() => navigation.navigate('Anúncio', { id_anuncio: item._id })}>
+            <Pressable key={index} onPress={() => navigation.navigate('Anúncio', { id_anuncio: item._id, id_anunciante: item.id_anunciante })}>
               <Image style={styles.cardImage} source={{uri : `https://jogueiros-api.herokuapp.com/uploads/${item.imagem}`}}></Image>
               <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
