1c6fbdcc0a5e4ed08eca9cde76933ae31b4f7d53
Rafael Viktor <rafaellviktor@gmail.com>
2022-11-03 20:13:31 -0300

    botão acessar tela de reservar

--- Jogueiros/views/anuncio.js
+++ Jogueiros/views/anuncio.js
@@ -75,7 +75,7 @@ function Anuncio({ route, navigation }) {
         <View style={styles.footer}>
           <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
             <Text style={styles.cardPrice}>{anuncio.preco} R$ /hora</Text>
-            <Button type='booking' title='Reservar' onpress={() => console.log('reservar')} style={{ width: '40%' }}/>
+            <Button type='booking' title='Reservar' onpress={() => navigation.navigate('Agendar reserva', { id_anuncio: route.params.id_anuncio, preco: anuncio.preco })}/>
           </View>
         </View>
       </View>
