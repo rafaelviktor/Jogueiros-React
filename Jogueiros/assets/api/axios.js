3f4a6836e10f8977ec07d0f1ffdd1d483eee094a
Rafael Viktor <rafaellviktor@gmail.com>
2022-12-03 14:23:42 -0300

    nova url api

--- Jogueiros/assets/api/axios.js
+++ Jogueiros/assets/api/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: 'https://jogueiros-api.herokuapp.com'});
+const instance = axios.create({baseURL: 'http://ec2-15-228-145-173.sa-east-1.compute.amazonaws.com:3333'});
 export default instance
