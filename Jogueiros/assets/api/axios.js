831a497f345017cc251bbe70a2cd7528ae5c3b0c
Rafael Viktor <rafaellviktor@gmail.com>
2022-12-03 16:18:43 -0300

    correção

--- Jogueiros/assets/api/axios.js
+++ Jogueiros/assets/api/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const instance = axios.create({baseURL: 'http://http://54.94.246.26:3333'});
+const instance = axios.create({baseURL: 'http://54.94.246.26:3333'});
 export default instance
